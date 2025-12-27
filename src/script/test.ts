2d7f490fe383a86ad88a18c2256ffe36a9a521fa
DmmDGM <dm12332131mdgaming@gmail.com>
2025-12-27 00:56:07 -0500

    woops

--- src/script/test.ts
+++
@@ -1,9 +0,0 @@
-// Imports
-import * as library from "../library";
-import { say } from "./pretty";
-
-// Tests origins
-const origins = await library.origins();
-const packets = await library.resolve(origins.map(async (origin) => await library.parse(origin)));
-const tested = await library.resolve(packets.map(async (packet) => await library.test(packet)));
-say(`Successfully tested ${tested.length} / ${origins.length} origins.`);
