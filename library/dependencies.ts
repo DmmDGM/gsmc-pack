9574d84cb27c0f43182ec633ae1e2795e591d26d
DmmDGM <dm12332131mdgaming@gmail.com>
2026-01-01 00:33:22 -0500

    reset

--- library/dependencies.ts
+++
@@ -1,9 +0,0 @@
-// Defines dependencies
-export interface Dependencies {
-    required: Record<string, boolean>;
-    conflict: Record<string, boolean>;
-    optional: Record<string, boolean>;
-}
-
-// Exports
-export default Dependencies;
